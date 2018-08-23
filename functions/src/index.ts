0c814ed4ea6c2a85ef2b339c1643f32e5713a617
Unknown <lordneru@gmail.com>
2018-08-24 00:54:11 +0200

    generate token for service provider role, with no event linked, and refactor functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,41 +1,67 @@
 import * as functions from 'firebase-functions';
+
 const randexp = require('randexp').randexp;
-const express = require('express')
 const admin = require('firebase-admin');
 admin.initializeApp();
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-app.get('/token', (req, res) => {
-    
-    const token = randexp('^[a-zA-Z0-9]{6}$');
-    
-    const eventid = req.query.eventid;
-    const role = req.query.role;
+app.use(cors({ origin: '*' }));
 
-    const tokenEntity = {
-        eventId: eventid,
-        role: role,
-        value: token,
-        timestamp: Date.now()/1000
-    };
+app.get('/token', async (req, res) => {
+  const token = randexp('^[a-zA-Z0-9]{6}$');
 
-    admin.firestore().collection('events').doc(eventid).get()
-        .then((result) => {
-            if(!result.exists) {
-                throw ('event id ' + eventid + ' does not exist');
-            }
-            return admin.firestore().collection('tokens').add(tokenEntity);
-        })
-        .then((result) => {
-            console.log('token ' + JSON.stringify(tokenEntity) + ' saved!');
-            res.send(token);
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(400);
-            res.send(err);
-        });
+  const role = req.query.role;
+  const eventid = req.query.eventid;
+
+  const tokenEntity = {
+    eventId: eventid,
+    role: parseInt(role),
+    value: token,
+    timestamp: Date.now() / 1000
+  };
+
+  console.log(tokenEntity);
+
+  try {
+    if (role === 4) {
+      delete tokenEntity.eventId;
+    } else {
+      const eventResult = await findEventById(eventid);
+
+      if (!eventResult.exists) {
+        throw 'event id ' + eventid + ' does not exist';
+      }
+      console.log('event data', eventResult.data());
+    }
+
+    await saveToken(tokenEntity);
+
+    console.log('token ' + JSON.stringify(tokenEntity) + ' saved!');
+
+    res.send({ token: token });
+  } catch (err) {
+    console.log(err);
+
+    res.status(400).send(err);
+  }
 });
+
+function findEventById(eventid: string) {
+  return admin
+    .firestore()
+    .collection('events')
+    .doc(eventid)
+    .get();
+}
+
+function saveToken(tokenEntity) {
+  return admin
+    .firestore()
+    .collection('tokens')
+    .add(tokenEntity);
+}
 
 exports.api = functions.https.onRequest(app);
